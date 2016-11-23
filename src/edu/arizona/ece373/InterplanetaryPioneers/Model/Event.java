3974a121e59589446f3dedf050583af087dd6760
Mike Harmon <mpharmon@email.arizona.edu>
2016-11-23 14:21:00 -0700

    Update Event and EventPool

--- src/edu/arizona/ece373/InterplanetaryPioneers/Model/Event.java
+++ src/edu/arizona/ece373/InterplanetaryPioneers/Model/Event.java
@@ -1,27 +1,35 @@
 package edu.arizona.ece373.InterplanetaryPioneers.Model;
 
+import java.awt.Color;
+import java.awt.Rectangle;
 import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
 
 import gameExecution.GameData;
 
 public class Event {
-	
-	protected String  title;
-	protected int 	  severity;
-	protected boolean Penalty;
-	private int 	  penaltyType;
-	protected int	  cost;
-	protected double  chance;
-	protected int 	  skillType;
+	protected String  	title;
+	protected int 	  	severity;
+	protected boolean 	Penalty;
+	protected int 	  	penaltyType;
+	protected int	  	cost;
+	protected double  	chance;
+	protected int 	  	skillType;
+	protected String  	description;
+	protected String  	option1;
+	protected String  	option2;
+	protected String  	option3;
+	protected String  	option4;
+	protected String  	gameOver;
+	protected String  	iconLocation;
+	protected Color	  	titleColor;
+	protected String  	eventText;
+	protected Rectangle iconBounds;
 	protected boolean Injury;		 //Crew Injury flag
 	protected boolean Damage;        //Ship Damage flag
 	protected boolean OverCondition; //Game over flag
-	protected String  description;
-	protected String  option1;
-	protected String  option2;
-	protected String  option3;
-	protected String  option4;
-	protected String  gameOver;   //Game over description
+
 	//Event severity definitions
 	public final static int Low = 1;
 	public final static int Moderate = 2;
@@ -43,6 +51,7 @@ public class Event {
 		option3 = "Assign vip to resolve issue...";
 		option4 = "Ignore problem...";
 		gameOver = "All critical events must have a game over description";
+		this.titleColor = new Color(0,255,255);
 	}
 	//Custom Constructor
 	public Event(int severity, boolean penalty, int penaltyType, int cost, double chance, int skillType, boolean injury, boolean damage) {
@@ -69,19 +78,47 @@ public class Event {
 		return this.title;
 	}
 
-	public String getDescription() {
-		return this.description;
+	public ImageIcon getIcon(){
+	//"/images/powerLoss.png"
+		return new ImageIcon(this.iconLocation);
 	}
+	public void 	setDescription(String d){this.description = d;}
+	public String 	getDescription(){return this.description;}
+
+	public void		setTitle(String t){this.title = t;}
+	
+	public void		setTitleColor(Color c){this.titleColor = c;}
+	public Color 	getTitleColor(){return this.titleColor;}
+	
+	public void 	setIconLocation(String loc){this.iconLocation = loc;}
+	public String 	getIconLocation(){return this.iconLocation;}
+	
+	public void		setIconBounds(int x, int y, int w, int h){this.iconBounds = new Rectangle(x,y,w,h);}
+	public Rectangle	getIconBounds(){return this.iconBounds;};
+	
+	public void 	setEventText(String t){this.eventText = t;}
+	public String 	getEventText(){return this.eventText;}
+	
+	public void		setOption1(String t){this.option1 = t;}
+	public String	getOption1(){return this.option1;}
+	
+	public void		setOption2(String t){this.option2 = t;}
+	public String	getOption2(){return this.option2;}
+	
+	public void		setOption3(String t){this.option3 = t;}
+	public String	getOption3(){return this.option3;}
+	
+	public void		setOption4(String t){this.option4 = t;}
+	public String	getOption4(){return this.option4;}
 	
 	public static String getTypeString(int type){
-		
 		if(type == GameData.fuel) return "Fuel";
 		else if(type == GameData.food) return "Food";
-		else if(type == GameData.water) return "water";
-		else if(type == GameData.parts) return "Spare parts";
-		
-		return null;
+		else if(type == GameData.water) return "Water";
+		else if(type == GameData.parts) return "Spare Parts";
+		else return "ERROR";
 	}
+<<<<<<< Updated upstream
 	public int getPenaltyType() {
 		return penaltyType;
 	}
@@ -89,4 +126,6 @@ public class Event {
 		this.penaltyType = penaltyType;
 	}
 	
+=======
+>>>>>>> Stashed changes
 }
--- src/edu/arizona/ece373/InterplanetaryPioneers/Model/EventPool.java
+++ src/edu/arizona/ece373/InterplanetaryPioneers/Model/EventPool.java
@@ -2,28 +2,32 @@ package edu.arizona.ece373.InterplanetaryPioneers.Model;
 
 import java.util.ArrayList;
 import gameExecution.GameData;
+import java.util.Random;
 
 public class EventPool {
-	
+	private static Random rand;
 	private static ArrayList<Event> events;
 	
 	public static void initEventPool(){
 		events = new ArrayList<Event>();
-		/**
-		 * Instantiate Event 1
-		 */
-		Event event1 = new Event(Event.Low, true, GameData.parts, 25, 0.50, Person.engineer, true, false);
+        
+        /*
+         *	Event #1 - Power Malfunction
+         */
+		newEvent = new Event(Event.Low, true, GameData.parts, 25, 0.50, Person.engineer);
 		//Set event text
-		event1.description = "The ship is experiencing minor power malfunctions. Currently the severity is low. You can resolve the issue\n "
-							+ "yourself or assign a crew member to resolve it. The decision is yours captain.";
+		newEvent.setDescription("The ship is experiencing minor power malfunctions. Currently the severity is low. You can resolve the issue\n "
+							+ "yourself or assign a crew member to resolve it. The decision is yours captain.");
 		//Set option text
-		event1.option1 = "[Success chance "+ event1.chance*100 +"%] Attempt to resolve power outtage youself.";
-		event1.option2 = "["+ String.valueOf(event1.cost - 15) + " "+ Event.getTypeString(event1.getPenaltyType()) +"] Assign an Engineer to fix the problem. The power outtage will be resolved at a minimal cost to resources.";
-		event1.option3 = "[5 - 25 "+ Event.getTypeString(event1.getPenaltyType()) +"] Assign your VIP to resolve the issue. Theres a chance your VIP will use less or more parts.";
-		event1.option4 = "The severity of the malfunction is low. You decide to ignore the problem for now.";
+		newEvent.setOption1("[Success chance "+ newEvent.chance*100 +"%] Attempt to resolve power outtage youself.");
+		newEvent.setOption2("["+ String.valueOf(newEvent.cost - 15) + " "+ Event.getTypeString(newEvent.penaltyType) +"] Assign an Engineer to fix the problem. The power outtage will be resolved at a minimal cost to resources.");
+		newEvent.setOption3("[5 - 25 "+ Event.getTypeString(newEvent.penaltyType) +"] Assign your VIP to resolve the issue. Theres a chance your VIP will use less or more parts.");
+		newEvent.setOption4("The severity of the malfunction is low. You decide to ignore the problem for now.");
 		
-		/**
-		 * Instantiate Event 2
+        EventPool.events.add(newEvent);
+        
+		/*
+		 *	Event #2 - Fire
 		 */
 		Event event2 = new Event(Event.Moderate, true, GameData.water, 15, 0.35, Person.scientist, true, false);
 		event2.description = ("There is a fire in the cargo area. The fire must be put out to avoid ship damage. You can resolve the issue\n "
@@ -33,17 +37,27 @@ public class EventPool {
 		event2.option2 = "["+ String.valueOf(event2.cost - 15) + " "+ Event.getTypeString(event2.getPenaltyType()) +"] Assign a Scientist to fix the problem. The fire will be resolved at no cost to resources.";
 		event2.option3 = "[0 - 15 "+ Event.getTypeString(event2.getPenaltyType()) +"] Assign your VIP to resolve the issue. Theres a chance your VIP will use less or more water.";
 		event2.option4 = "You hope nothing will go wrong and ignore the fire.";
-		/**
-		 * Add all created events to ArrayList
-		 */
-		events.add(event1);
-		events.add(event2);
-		//...
-		//...
-		//Events.add(eventN);
-	}
-	public static Event getEvent(int select){
-		return events.get(select);
-	}
 
+		newEvent = new Event(Event.Moderate, true, GameData.water, 15, 0.35, Person.scientist);
+		newEvent.setTitle("FIRE!");
+		newEvent.setDescription("There is a fire in the cargo area. The fire must be put out to avoid ship damage. You can resolve the issue\n "
+							+ "yourself or assign a crew member to resolve it. The decision is yours captain.");
+		//Set option text
+		newEvent.setOption1("[Success chance "+ newEvent.chance*100 +"%] Attempt to resolve fire youself.");
+		newEvent.setOption2("["+ String.valueOf(newEvent.cost - 15) + " "+ Event.getTypeString(newEvent.penaltyType) +"] Assign a Scientist to fix the problem. The fire will be resolved at no cost to resources.");
+		newEvent.setOption3("[0 - 15 "+ Event.getTypeString(newEvent.penaltyType) +"] Assign your VIP to resolve the issue. Theres a chance your VIP will use less or more water.");
+		newEvent.setOption4("You hope nothing will go wrong and ignore the fire.");
+		
+        EventPool.events.add(newEvent);
+        
+        /*
+         *	Event #3 - 
+         */
+		newEvent = new Event(Event.Moderate, true, GameData.water, 15, 0.35, Person.engineer);
+		newEvent.setDescription("");
+	}
+    
+    public static Event getRandomEvent(){
+    	return EventPool.events.get(rand.nextInt((EventPool.events.size() - 1)));
+    }
 }
